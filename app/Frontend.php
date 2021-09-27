e8bfa99bd4e47730c4e98bc1800f7e13bdb3f30e
Tom Broucke <tom@tombroucke.be>
2021-09-27 11:38:36 +0200

    initial commit

---
+++ app/Frontend.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Otomaties\PluginBoilerplate;
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @subpackage PluginBoilerplate/public
+ */
+
+class Frontend
+{
+
+    /**
+     * The ID of this plugin.
+     *
+     * @var      string    $pluginName    The ID of this plugin.
+     */
+    private $pluginName;
+
+    /**
+     * The version of this plugin.
+     *
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @param      string    $pluginName       The name of the plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct($pluginName, $version)
+    {
+
+        $this->pluginName = $pluginName;
+        $this->version = $version;
+    }
+
+    /**
+     * Register the stylesheets for the public-facing side of the site.
+     *
+     */
+    public function enqueueStyles()
+    {
+
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
+        wp_enqueue_style($this->pluginName, plugin_dir_url(__FILE__) . 'css/plugin-name-public.css', array(), $this->version, 'all');
+    }
+
+    /**
+     * Register the JavaScript for the public-facing side of the site.
+     *
+     */
+    public function enqueueScripts()
+    {
+
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
+
+        wp_enqueue_script($this->pluginName, plugin_dir_url(__FILE__) . 'js/plugin-name-public.js', array( 'jquery' ), $this->version, false);
+    }
+}
---
+++ app/Plugin.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Otomaties\PluginBoilerplate;
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ */
+
+class Plugin
+{
+
+    /**
+     * The loader that's responsible for maintaining and registering all hooks that power
+     * the plugin.
+     *
+     * @var      Loader    $loader    Maintains and registers all hooks for the plugin.
+     */
+    protected $loader;
+
+    /**
+     * The current version of the plugin.
+     *
+     * @var      string    $version    The current version of the plugin.
+     */
+    protected $version;
+
+    /**
+     * The name of the plugin
+     *
+     * @var [type]
+     */
+    protected $pluginName;
+
+    /**
+     * Define the core functionality of the plugin.
+     *
+     * Set the plugin name and the plugin version that can be used throughout the plugin.
+     * Load the dependencies, define the locale, and set the hooks for the admin area and
+     * the public-facing side of the site.
+     *
+     */
+    public function __construct($pluginData)
+    {
+        $this->version = $pluginData['Version'];
+        $this->pluginName = $pluginData['pluginName'];
+        $this->loader = new Loader();
+
+        $this->setLocale();
+        $this->defineAdminHooks();
+        $this->defineFrontendHooks();
+    }
+
+    /**
+     * Define the locale for this plugin for internationalization.
+     *
+     * Uses the i18n class in order to set the domain and to register the hook
+     * with WordPress.
+     *
+     */
+    private function setLocale()
+    {
+
+        $plugin_i18n = new I18n();
+
+        $this->loader->add_action('plugins_loaded', $plugin_i18n, 'loadTextdomain');
+    }
+
+    /**
+     * Register all of the hooks related to the admin-facing functionality
+     * of the plugin.
+     *
+     */
+    private function defineAdminHooks()
+    {
+
+        $admin = new Admin($this->getPluginName(), $this->getVersion());
+        $this->loader->add_action('admin_enqueue_scripts', $admin, 'enqueueStyles');
+        $this->loader->add_action('admin_enqueue_scripts', $admin, 'enqueueScripts');
+    }
+
+    /**
+     * Register all of the hooks related to the public-facing functionality
+     * of the plugin.
+     *
+     */
+    private function defineFrontendHooks()
+    {
+        $frontend = new Frontend($this->getPluginName(), $this->getVersion());
+        $this->loader->add_action('wp_enqueue_scripts', $frontend, 'enqueueStyles');
+        $this->loader->add_action('wp_enqueue_scripts', $frontend, 'enqueueScripts');
+    }
+
+    /**
+     * Run the loader to execute all of the hooks with WordPress.
+     *
+     */
+    public function run()
+    {
+        $this->loader->run();
+    }
+
+    /**
+     * The reference to the class that orchestrates the hooks with the plugin.
+     *
+     * @since     1.0.0
+     * @return    Loader    Orchestrates the hooks of the plugin.
+     */
+    public function getLoader()
+    {
+        return $this->loader;
+    }
+
+    public function getPluginName() {
+        return $this->pluginName;
+    }
+
+    /**
+     * Retrieve the version number of the plugin.
+     *
+     * @since     1.0.0
+     * @return    string    The version number of the plugin.
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+}
