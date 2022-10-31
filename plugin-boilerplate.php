70abcbf7844ed0df6d88f517abc64d9f3116d3a1
tombroucke <tom@tombroucke.be>
2022-10-31 16:56:25 +0100

    Update plugin-boilerplate.php

--- plugin-boilerplate.php
+++ plugin-boilerplate.php
@@ -8,7 +8,7 @@ namespace Otomaties\PluginBoilerplate;
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Tom Broucke
- * Author URI:        http://tombroucke.be/
+ * Author URI:        https://tombroucke.be/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-boilerplate
